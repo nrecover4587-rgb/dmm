75895216a7c3134ddcd9ba91f34b9794feddf338
nrecover4587-rgb <nrecover4587@gmail.com>
2026-04-07 04:51:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 29245660
 API_HASH = "0c2db11dd0f4075e8d0f2f81f240be91"
 
-BOT_TOKEN = "859706664:AAE_Ov0AZX3hkHtAPjo2FF56k8BFUU1MazI"
+BOT_TOKEN = "8597060664:AAE_Ov0AZX3hkHtAPjo2FF56k8BFUU1MazI"
 
 MONGO_URL = "mongodb+srv://PurviBots:PublicMongo@cluster0.gy2adez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
