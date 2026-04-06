d90c4fd6fa073bdf925f71060a05a22b22699ef3
nrecover4587-rgb <nrecover4587@gmail.com>
2026-04-07 04:45:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 29245660
+API_HASH = "0c2db11dd0f4075e8d0f2f81f240be91"
+
+BOT_TOKEN = "859706664:AAE_Ov0AZX3hkHtAPjo2FF56k8BFUU1MazI"
+
+MONGO_URL = "mongodb+srv://PurviBots:PublicMongo@cluster0.gy2adez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DM_LOGGER_ID = -1003723987338
+OWNER_IDS = [7113972959]
